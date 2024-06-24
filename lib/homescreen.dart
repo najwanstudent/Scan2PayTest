75165965f5835c7a7c655ce3bb1eea2940b04de2
JarodJill <jarod.jumin@yahoo.com>
2024-06-25 02:58:44 +0800

    homescreen UI

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -45,50 +45,157 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text('Scan2Pay'),
       ),
       drawer: AppDrawer(userUid: widget.userUid), // Pass userUid to AppDrawer
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Current Balance',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      body: Stack(
+        children: [
+          // Background image
+          Positioned.fill(
+            child: Image.asset(
+              'assets/white.jpg', // Replace with your main background image path
+              fit: BoxFit.cover,
             ),
-            StreamBuilder<DocumentSnapshot>(
-              stream: FirebaseFirestore.instance
-                  .collection('Users')
-                  .doc(widget.userUid)
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return const Text(
-                    '\$0',
-                    style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-                  );
-                }
-                var userDoc = snapshot.data!;
-                num balance = userDoc['amount_balance'];
+          ),
+          // Top third background color with custom painter
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            height: MediaQuery.of(context).size.height / 3, // One-third of the screen
+            child: CustomPaint(
+              painter: TopContainerPainter(),
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Stack(
+                      children: [
+                        Text(
+                          'Current Balance',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            foreground: Paint()
+                              ..style = PaintingStyle.stroke
+                              ..strokeWidth = 2
+                              ..color = Colors.black,
+                          ),
+                        ),
+                        const Text(
+                          'Current Balance',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    StreamBuilder<DocumentSnapshot>(
+                      stream: FirebaseFirestore.instance
+                          .collection('Users')
+                          .doc(widget.userUid)
+                          .snapshots(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return Stack(
+                            children: [
+                              Text(
+                                '\$0',
+                                style: TextStyle(
+                                  fontSize: 48,
+                                  fontWeight: FontWeight.bold,
+                                  foreground: Paint()
+                                    ..style = PaintingStyle.stroke
+                                    ..strokeWidth = 2
+                                    ..color = Colors.black,
+                                ),
+                              ),
+                              const Text(
+                                '\$0',
+                                style: TextStyle(
+                                  fontSize: 48,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          );
+                        }
+                        var userDoc = snapshot.data!;
+                        num balance = userDoc['amount_balance'];
 
-                return Text(
-                  '\RM$balance',
-                  style: const TextStyle(
-                      fontSize: 48, fontWeight: FontWeight.bold),
-                );
-              },
+                        return Stack(
+                          children: [
+                            Text(
+                              '\RM$balance',
+                              style: TextStyle(
+                                fontSize: 48,
+                                fontWeight: FontWeight.bold,
+                                foreground: Paint()
+                                  ..style = PaintingStyle.stroke
+                                  ..strokeWidth = 2
+                                  ..color = Colors.black,
+                              ),
+                            ),
+                            Text(
+                              '\RM$balance',
+                              style: const TextStyle(
+                                fontSize: 48,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ),
-            const SizedBox(height: 20),
-            QrImageView(
-              data: '$icNumber', // Include balance in the QR data
-              version: QrVersions.auto,
-              size: 200.0,
+          ),
+          // Content overlay
+          Positioned(
+            top: MediaQuery.of(context).size.height / 3,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // Container with white background and shadow for QR code
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    padding: const EdgeInsets.all(20),
+                    child: QrImageView(
+                      data: '$icNumber', // Include balance in the QR data
+                      version: QrVersions.auto,
+                      size: 200.0,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () => _printQr(context),
+                    child: const Text('PRINT QR'),
+                  ),
+                  const SizedBox(height: 20),
+                ],
+              ),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _printQr(context),
-              child: const Text('PRINT QR'),
-            ),
-            const SizedBox(height: 20),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -135,5 +242,35 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
     );
+  }
+}
+
+class TopContainerPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..shader = LinearGradient(
+        colors: [Colors.purple, Colors.deepPurpleAccent],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+      ).createShader(Rect.fromLTWH(0, 0, size.width, size.height))
+      ..style = PaintingStyle.fill;
+
+    var path = Path()
+      ..moveTo(0, 0)
+      ..lineTo(size.width, 0)
+      ..lineTo(size.width, size.height - 40)
+      ..quadraticBezierTo(
+        0, size.height,
+        0, size.height - 40,
+      )
+      ..close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
